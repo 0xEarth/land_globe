b9ac1f5623e92197a54e9ee1975c5b1e2dafc828
David Hodge <dhodge.jr229@gmail.com>
2020-04-13 16:30:50 -0400

    Updates

---
+++ precache-manifest.820549de9a2b46945914ceb0c33fcaad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e3eee39abafcc6b9c13",
+    "url": "/land_globe/static/css/main.c5f4e83c.chunk.css"
+  },
+  {
+    "revision": "2e3eee39abafcc6b9c13",
+    "url": "/land_globe/static/js/main.2e3eee39.chunk.js"
+  },
+  {
+    "revision": "cfaf4b76f473f5dde3b9",
+    "url": "/land_globe/static/js/1.cfaf4b76.chunk.js"
+  },
+  {
+    "revision": "1874ea480084df37ed0d",
+    "url": "/land_globe/static/js/runtime~main.1874ea48.js"
+  },
+  {
+    "revision": "5bc9de028d0c30d6d9e4f70d0978ef4c",
+    "url": "/land_globe/index.html"
+  }
+];
